7edba45fb959dea8f6d3d142dce4a608aefee96a
Yagunove <yagusei.jr@gmail.com>
2017-12-18 15:12:04 +0300

    Исправление последствий слияния Часть 3

--- CurrenZYbot/telegram-bot/index.js
+++
@@ -1,8 +0,0 @@
-﻿var TelegramBot = require('node-telegram-bot-api');
-
-// Устанавливаем токен, который выдавал нам бот.
-var token = '481988096:AAFgLQMQPgN-8mFnn2adPh-cQla2K0S3ugI';
-// Включить опрос сервера
-var bot = new TelegramBot(token, { polling: true });
-
-
